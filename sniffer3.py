99bbc55fda8545be7c4ccf694536ae2286e34352
Caliburn556 <isaac.kipruto@strathmore.edu>
2024-06-22 10:32:29 +0300

    sniffer3.py  can identify  two mac address claiming to be one MAC address

--- sniffer3.py
+++ sniffer3.py
@@ -1,37 +1,35 @@
 #! /usr/bin/env python3
 from scapy.all import ARP, sniff
-from collections import defaultdict
 
 # Dictionary to store IP to MAC mappings
-arp_table = defaultdict(set)
+ip_mac_mapping = {}
 
 def arp_display(pkt):
     if ARP in pkt and pkt[ARP].op == 2:  # is-at (response)
         source_ip = pkt[ARP].psrc
         source_mac = pkt[ARP].hwsrc
+        destination_mac = pkt[ARP].hwdst
+        protocol = 'ARP'
+        length = len(pkt)
+        info = f"{source_ip} is at {source_mac}"
 
-        # Update the ARP table
-        arp_table[source_ip].add(source_mac)
+        # Label as broadcast if destination MAC is 00:00:00:00:00:00
+        if destination_mac == '00:00:00:00:00:00':
+            destination_mac = 'Broadcast'
 
-        # Detect ARP spoofing
-        if len(arp_table[source_ip]) > 1:
-            print(f"WARNING: Possible ARP spoofing detected for IP {source_ip}!")
-            print(f"IP address {source_ip} is being claimed by multiple MAC addresses: {', '.join(arp_table[source_ip])}")
+        # Signature-based detection for ARP spoofing
+        if source_ip in ip_mac_mapping:
+            if source_mac not in ip_mac_mapping[source_ip]:
+                ip_mac_mapping[source_ip].add(source_mac)
+                print(f"WARNING: Possible ARP spoofing detected!")
+                print(f"IP address {source_ip} is being claimed by multiple MAC addresses: {', '.join(ip_mac_mapping[source_ip])}")
         else:
-            print(f"ARP entry: {source_ip} is at {source_mac}")
+            ip_mac_mapping[source_ip] = {source_mac}
 
-def print_arp_table():
-    print("\nCurrent ARP Table:")
-    print(f"{'IP Address':15} {'MAC Addresses'}")
-    print("="*40)
-    for ip, macs in arp_table.items():
-        print(f"{ip:15} {', '.join(macs)}")
+        print(f"{source_mac:20} {destination_mac:20} {protocol:10} {length:6} {info}")
 
 # Print the header
 print(f"{'Source MAC':20} {'Destination MAC':20} {'Protocol':10} {'Length':6} {'Info'}")
 
-try:
-    # Start sniffing, filtering only ARP responses
-    sniff(prn=arp_display, filter="arp", store=0)
-except KeyboardInterrupt:
-    print_arp_table()
+# Start sniffing, filtering only ARP responses
+sniff(prn=arp_display, filter="arp", store=0)
